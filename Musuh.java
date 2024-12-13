324aa9573fda3d1598710fe0a861004f82d6c884
Indriazanalka <151296105+Indriazanalka@users.noreply.github.com>
2024-12-13 21:58:00 +0700

    Update Musuh.java

--- Musuh.java
+++ Musuh.java
@@ -1,1 +1,16 @@
+package game;
 
+import javafx.geometry.Rectangle2D;
+
+public class Musuh extends Karakter {
+    private int speed; 
+
+    public Musuh(int x, int y, int speed) {
+        super(x, y, 1); 
+        this.speed = speed; 
+    }
+
+    public Rectangle2D getBounds() {
+        return new Rectangle2D(x, y, 50, 50); 
+    }
+}
