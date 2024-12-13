311bdeaad8ffb1680b120c6f74c1a7e7ad03360d
Indriazanalka <151296105+Indriazanalka@users.noreply.github.com>
2024-12-13 23:59:16 +0700

    Update Musuh.java

--- Musuh.java
+++ Musuh.java
@@ -4,12 +4,14 @@ import javafx.geometry.Rectangle2D;
 
 public class Musuh extends Karakter {
     private int speed; 
-
+    
+  // Konstruktor untuk Musuh
     public Musuh(int x, int y, int speed) {
         super(x, y, 1); 
         this.speed = speed; 
     }
-
+    
+  // Metode untuk mendapatkan batasan (bounding box) musuh
     public Rectangle2D getBounds() {
         return new Rectangle2D(x, y, 50, 50); 
     }
